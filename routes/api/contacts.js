8fee0e25e082e9052ecaa223e82af2f2d0ea230b
IraMira333 <irinat333@gmail.com>
2023-09-05 16:09:13 +0300

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,16 +1,25 @@
 import express from "express";
 import contactsControllers from "../../controllers/contactsControllers.js";
+import contactValidation from "../../middleware/validation/contactValidation.js";
 
 const contactsRouter = express.Router();
 
-contactsRouter.get("/", contactsControllers.getAll());
+contactsRouter.get("/", contactsControllers.getAll);
 
-contactsRouter.get("/:id", contactsControllers.getById());
+contactsRouter.get("/:id", contactsControllers.getById);
 
-contactsRouter.post("/", contactsControllers.add());
+contactsRouter.post(
+  "/",
+  contactValidation.contactValidate,
+  contactsControllers.add
+);
 
-contactsRouter.delete("/:id", contactsControllers.removeById());
+contactsRouter.delete("/:id", contactsControllers.removeById);
 
-contactsRouter.put("/:id", contactsControllers.updateById());
+contactsRouter.put(
+  "/:id",
+  contactValidation.contactValidate,
+  contactsControllers.updateById
+);
 
 export default contactsRouter;
