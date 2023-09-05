af9530f41538a3f57411a525f8d688efbbc49f55
IraMira333 <irinat333@gmail.com>
2023-09-05 15:32:33 +0300

    Create contactsControllers.js

---
+++ controllers/contactsControllers.js
@@ -0,0 +1,63 @@
+import Joi from "joi";
+import contactsService from "../models/contacts.js";
+import { HttpError } from "../helpers/index.js";
+import ctrlWrapper from "../decorators/index.js";
+
+const contactAddSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+});
+
+const getAll = async (req, res) => {
+  const allContacts = await contactsService.listContacts();
+  res.status(200).json(allContacts);
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const contactById = await contactsService.getContactById(id);
+  if (!contactById) {
+    throw HttpError(404, `Contact with id=${id} not found`);
+  }
+  res.status(200).json(contactById);
+};
+
+const add = async (req, res) => {
+  const { error } = contactAddSchema.validate(req.body);
+  if (error) {
+    throw HttpError(400, error.message);
+  }
+  const addedContact = await contactsService.addContact(req.body);
+  res.status(201).json(addedContact);
+};
+
+const removeById = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsService.removeContact(id);
+  if (!result) {
+    throw HttpError(404, `Contact with id=${id} not found`);
+  }
+  res.status(200).json({ message: "contact deleted" });
+};
+
+const updateById = async (req, res) => {
+  const { error } = contactAddSchema.validate(req.body);
+  if (error) {
+    throw HttpError(400, error.message);
+  }
+  const { id } = req.params;
+  const result = await contactsService.updateContact(id, req.body);
+  if (!result) {
+    throw HttpError(404, `Contact with id=${id} not found`);
+  }
+  res.status(200).json(result);
+};
+
+export default {
+  getAll: ctrlWrapper(getAll),
+  add: ctrlWrapper(add),
+  getById: ctrlWrapper(getById),
+  removeById: ctrlWrapper(removeById),
+  updateById: ctrlWrapper(updateById),
+};
