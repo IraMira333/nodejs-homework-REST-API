15ef6d9fc0f39134cc4d39f4a02cb198368d283a
IraMira333 <irinat333@gmail.com>
2023-09-08 15:38:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+import mongoose from "mongoose";
+
 import app from "./app.js";
-//VBO06VadYoD2BZdH
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+
+const DB_HOST =
+  "mongodb+srv://Irina:VBO06VadYoD2BZdH@cluster0.l0odltx.mongodb.net/ContactsData?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
