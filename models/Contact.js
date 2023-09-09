3c985a09ad7a996c15f1b7f01d4f4e8561fd928e
IraMira333 <irinat333@gmail.com>
2023-09-09 23:14:53 +0300

    Update Contact.js

--- models/Contact.js
+++ models/Contact.js
@@ -1,10 +1,25 @@
 import { Schema, model } from "mongoose";
+import { handleSaveError } from "./hooks.js";
 
 const contactSchema = new Schema({
-  name: String,
-  email: String,
-  phone: String,
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+    require: true,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
 });
+
+contactSchema.post("save", handleSaveError);
 
 const Contact = model("contact", contactSchema);
 
