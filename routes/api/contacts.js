6abd7310f3cba53954809b59e55fe65be941e4a6
IraMira333 <irinat333@gmail.com>
2023-09-10 00:44:30 +0300

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,33 @@
 import express from "express";
 import contactsControllers from "../../controllers/contactsControllers.js";
 import contactValidation from "../../middleware/validation/contactValidation.js";
+import isValidId from "../../middleware/isValidId.js";
 
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", contactsControllers.getAll);
 
-// contactsRouter.get("/:id", contactsControllers.getById);
+contactsRouter.get("/:id", isValidId, contactsControllers.getById);
 
-// contactsRouter.post(
-//   "/",
-//   contactValidation.contactValidate,
-//   contactsControllers.add
-// );
+contactsRouter.post(
+  "/",
+  contactValidation.contactValidate,
+  contactsControllers.add
+);
 
-// contactsRouter.delete("/:id", contactsControllers.removeById);
+contactsRouter.delete("/:id", isValidId, contactsControllers.removeById);
 
-// contactsRouter.put(
-//   "/:id",
-//   contactValidation.contactValidate,
-//   contactsControllers.updateById
-// );
+contactsRouter.put(
+  "/:id",
+  isValidId,
+  contactValidation.contactValidate,
+  contactsControllers.updateById
+);
+contactsRouter.patch(
+  "/:id/favorite",
+  isValidId,
+  contactValidation.contactFavoritValidate,
+  contactsControllers.updateStatusContact
+);
 
 export default contactsRouter;
