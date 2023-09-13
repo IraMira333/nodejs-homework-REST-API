e216a09d3380bd2d185c74be99795d3de6088a31
IraMira333 <irinat333@gmail.com>
2023-09-13 16:26:33 +0300

    Update Contact.js

--- models/Contact.js
+++ models/Contact.js
@@ -1,4 +1,6 @@
 import { Schema, model } from "mongoose";
+import Joi from "joi";
+
 import { handleSaveError, runValidateAtUpdate } from "./hooks.js";
 
 const contactSchema = new Schema(
@@ -25,6 +27,17 @@ const contactSchema = new Schema(
 contactSchema.post("save", handleSaveError);
 contactSchema.pre("findOneAndUpdate", runValidateAtUpdate);
 contactSchema.post("findOneAndUpdate", handleSaveError);
+
+export const contactAddSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string(),
+  phone: Joi.string().required(),
+  favorite: Joi.boolean().default(false),
+});
+
+export const contactUpdateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean(),
+});
 
 const Contact = model("contact", contactSchema);
 
