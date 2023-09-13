355a10dcf976ea87b742da662e7229348cc22f92
IraMira333 <irinat333@gmail.com>
2023-09-13 16:26:31 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ import express from "express";
 import logger from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
-import contactsRouter from "./routes/api/contacts.js";
+import authRouter from "./routes/api/authRouter.js";
+import contactsRouter from "./routes/api/contactsRouter.js";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
