2a4908287ec08444f4ea8184b7cb48748267d7b6
IraMira333 <irinat333@gmail.com>
2023-09-13 23:17:18 +0300

    Update authRouter.js

--- routes/api/authRouter.js
+++ routes/api/authRouter.js
@@ -1,5 +1,11 @@
 import express from "express";
+import authControllers from "../../controllers/authControllers.js";
+import * as userSchemas from "../../models/User.js";
+import { validateBody } from "../../decorators/index.js";
 
 const authRouter = express.Router();
+const userSingUpValidate = validateBody(userSchemas.userSingUpSchema);
+
+authRouter.post("/singup", userSingUpValidate, authControllers.singUp);
 
 export default authRouter;
