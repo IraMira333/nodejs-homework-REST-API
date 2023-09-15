e1a8b5bb1c752a3e3b68c59df4bf5da93906cfd0
IraMira333 <irinat333@gmail.com>
2023-09-16 00:30:50 +0300

    Update contactValidation.js

--- middleware/validation/contactValidation.js
+++ middleware/validation/contactValidation.js
@@ -1,4 +1,4 @@
-import contactsSchemas from "../../models/Contact.js";
+import * as contactsSchemas from "../../models/Contact.js";
 import { validateBody } from "../../decorators/index.js";
 
 const contactValidate = validateBody(contactsSchemas.contactAddSchema);
