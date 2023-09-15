7307b8e2d56176f15949ac8993a9cc0e423b5dd2
IraMira333 <irinat333@gmail.com>
2023-09-15 23:14:16 +0300

    Update authControllers.js

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,20 +1,37 @@
+import bcrypt from "bcryptjs";
 import User from "../models/User.js";
 import { HttpError } from "../helpers/index.js";
 import { ctrlWrapper } from "../decorators/index.js";
 
 const singUp = async (req, res) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (user) {
     throw HttpError(409, "Email already exist");
   }
-  const newUser = await User.create(req.body);
+  const hashPassword = await bcrypt.hash(password, 10);
+  const newUser = await User.create({ ...req.body, password: hashPassword });
   res.status(201).json({
     username: newUser.username,
     email: newUser.email,
   });
 };
 
+const singIn = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password invalid");
+  }
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password invalid");
+  }
+  const token = "Make a token";
+  res.status(200).json({ token, user: { email, subscription } });
+};
+
 export default {
   singUp: ctrlWrapper(singUp),
+  singIn: ctrlWrapper(singIn),
 };
