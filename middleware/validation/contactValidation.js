e4b12a16cd26f860a9da4dccea9723dac756fc00
IraMira333 <irinat333@gmail.com>
2023-09-16 23:22:32 +0300

    Delete contactValidation.js

--- middleware/validation/contactValidation.js
+++
@@ -1,9 +0,0 @@
-import * as contactsSchemas from "../../models/Contact.js";
-import { validateBody } from "../../decorators/index.js";
-
-const contactValidate = validateBody(contactsSchemas.contactAddSchema);
-const contactFavoritValidate = validateBody(
-  contactsSchemas.contactUpdateFavoriteSchema
-);
-
-export default { contactValidate, contactFavoritValidate };
