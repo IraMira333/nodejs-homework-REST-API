77616863d03c5476fd8483b0ddea7a0492bba617
IraMira333 <irinat333@gmail.com>
2023-09-16 23:57:02 +0300

    Update authenticate.js

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,6 +1,26 @@
 import jwt from "jsonwebtoken";
+import { HttpError } from "../helpers/index.js";
+import { ctrlWrapper } from "../decorators/index.js";
+import User from "../models/User.js";
 
 const { JWT_SECRET } = process.env;
 
-const authenticate = async (req, res, next) => {};
-export default authenticate;
+const authenticate = async (req, res, next) => {
+  const { authorization } = req.headers;
+  console.log(process.env.JWT_SECRET);
+  const [bearer, token] = authorization.split(" ");
+  if (bearer === "Bearer") {
+    throw HttpError(401);
+  }
+  try {
+    const { id } = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(id);
+    if (!user) {
+      throw HttpError(401);
+    }
+    next();
+  } catch {
+    throw HttpError(401);
+  }
+};
+export default ctrlWrapper(authenticate);
