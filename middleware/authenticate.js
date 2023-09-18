9da5b174a6b507adf240f3cb7cf26e8a98b301e1
IraMira333 <irinat333@gmail.com>
2023-09-19 00:15:47 +0300

    Update authenticate.js

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -3,24 +3,27 @@ import { HttpError } from "../helpers/index.js";
 import { ctrlWrapper } from "../decorators/index.js";
 import User from "../models/User.js";
 
-const { JWT_SECRET } = process.env;
-
 const authenticate = async (req, res, next) => {
-  const { authorization } = req.headers;
-  console.log(process.env.JWT_SECRET);
+  const { authorization = " " } = req.headers;
+
   const [bearer, token] = authorization.split(" ");
-  if (bearer === "Bearer") {
+
+  if (bearer !== "Bearer") {
     throw HttpError(401);
   }
   try {
-    const { id } = jwt.verify(token, process.env.JWT_SECRET);
+    const { JWT_SECRET } = process.env;
+
+    const { id } = jwt.verify(token, JWT_SECRET);
     const user = await User.findById(id);
-    if (!user) {
-      throw HttpError(401);
+
+    if (!user || !user.token) {
+      throw HttpError(401, "Not authorized");
     }
+    req.user = user;
     next();
   } catch {
-    throw HttpError(401);
+    throw HttpError(401, "Not authorized");
   }
 };
 export default ctrlWrapper(authenticate);
