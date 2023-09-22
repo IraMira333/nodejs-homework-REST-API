c0bbafc8a2c5dac01d5918ba8464930f8036f614
IraMira333 <irinat333@gmail.com>
2023-09-23 00:17:19 +0300

    Update upload.js

--- middleware/upload.js
+++ middleware/upload.js
@@ -6,9 +6,9 @@ const destination = path.resolve("temp");
 const storage = multer.diskStorage({
   destination,
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    const filename = `${uniqueSuffix}_${file.originalname}`;
-    cb(null, filename);
+    // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    // const filename = `${uniqueSuffix}_${file.originalname}`;
+    cb(null, file.originalname);
   },
 });
 
