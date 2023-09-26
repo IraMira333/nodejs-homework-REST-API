81674b59901bfe1cba45caa68b8088eaa2194e98
IraMira333 <irinat333@gmail.com>
2023-09-26 23:48:39 +0300

    Update authRouter.test.js

--- routes/api/authRouter.test.js
+++ routes/api/authRouter.test.js
@@ -18,21 +18,14 @@ describe("test login route", () => {
     server.close();
   });
 
-  // afterEach(async () => {
-  //   await User.deleteMany({});
-  // });
-
   test("test login with correct data", async () => {
     const loginData = {
-      //username: "Margo7",
       email: "Margosha4@gmail.com",
       password: "Margosha123",
     };
     const { statusCode, body } = await request(app)
       .post("/api/users/login")
       .send(loginData);
-    console.log(statusCode);
-    console.log(body);
 
     const user = await User.findOne({ email: loginData.email });
     expect(user.email).toBe(loginData.email);
